5bd8811771945e325dd9d3c2a83e90b1dc28fe23
Paul Anthony McGowan <xhostcom@aol.com>
2022-12-28 08:29:14 +0000

    Redis cache settings and session

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,7 +40,8 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
-    config.cache_store = :memory_store
+    config.cache_store = :redis_cache_store, { url: ENV.fetch("REDIS_URL") { "redis://localhost:6379/1" } }
+    config.session_store :cache_store, key: "_sessions_development", compress: true, pool_size: 5, expire_after: 1.year
     config.public_file_server.headers = {
       "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }

b0333b9f52e48dd5b8dc5e328935c562ec94487b
Paul Anthony McGowan <xhostcom@aol.com>
2023-01-03 10:14:25 +0000

    before action controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, unless: :devise_controller?
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
